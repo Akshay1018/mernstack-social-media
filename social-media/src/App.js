76356cef2b5b4d51b41f023dd5090417fdc97834
akshay1018 <ak715173@gmail.com>
2021-06-12 14:47:12 +0530

    added posts and form component

--- social-media/src/App.js
+++ social-media/src/App.js
@@ -1,22 +1,52 @@
 import React from 'react'
-import Register from './components/Register.js';
-import Login from './components/Login'
-import { Route, BrowserRouter as Router, Switch ,Link} from 'react-router-dom';
+
+import { AppBar, Typography, Container, Grow, Grid } from '@material-ui/core';
+import memories from './components/images/memory.jpg';
+import Posts from './components/posts';
+import Form from './components/form'
+// import Register from './components/Register.js';
+// import Login from './components/Login'
+// import { Route, BrowserRouter as Router, Switch, Link } from 'react-router-dom';
 function App() {
     return (
-        <Router>
-            <Switch>
-                <Route exact path='/register' component={Register} />
-                <Route exact path='/login' component={Login} />
-                <Route component={
-                    () => (
-                        <h1>Not Found.
-                                Go Back <Link to='/'>Home Page</Link>
-                        </h1>
-                    )
-                } />
-            </Switch>
-        </Router>
+        <Container maxWidth="lg">
+            <AppBar position="static" color="inherit">
+                <Typography variant="h2" align='center'>
+
+
+                    Along the Way…
+               </Typography>
+                <img src={memories} alt='memories' height='60' />
+
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justify='space-between' alignItems='stretch' spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts/>
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form/>
+                        </Grid>
+
+                    </Grid>
+                </Container>
+            </Grow>
+
+        </Container>
+        // <Router>
+        //     <Switch>
+        //         <Route exact path='/register' component={Register} />
+        //         <Route exact path='/login' component={Login} />
+        //         <Route component={
+        //             () => (
+        //                 <h1>Not Found.
+        //                         Go Back <Link to='/'>Home Page</Link>
+        //                 </h1>
+        //             )
+        //         } />
+        //     </Switch>
+        // </Router>
 
     )
 }
---
+++ social-media/src/components/posts.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Post from './post'
+
+function posts() {
+    return (
+        <div>
+            <h1>Posts</h1>
+            <Post/>
+            <Post/>
+        </div>
+    )
+}
+
+export default posts
