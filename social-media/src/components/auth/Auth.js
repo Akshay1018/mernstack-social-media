64f03c61da4d8fb15d2d94829d6d23e375395b9e
akshay1018 <ak715173@gmail.com>
2021-06-17 11:52:11 +0530

    sign in with google functionality added

--- social-media/src/components/auth/Auth.js
+++ social-media/src/components/auth/Auth.js
@@ -2,13 +2,14 @@ import React from 'react'
 import { Avatar, Container, Button, Grid, Paper, Typography } from "@material-ui/core"
 import useStyles from './styles'
 import LockIcon from '@material-ui/icons/LockOutlined'
-import TextField from '@material-ui/core/TextField';
+import { GoogleLogin } from 'react-google-login'
 import Input from './Input';
 import { useState } from 'react';
+import Icon from './Icon'
 function Auth() {
     const classes = useStyles();
-    const [isSignUp,setisSignup] = useState(false);
-  
+    const [isSignUp, setisSignup] = useState(false);
+
     const [showPassword, setshowPassword] = useState(false);
 
     const onsubmit = () => {
@@ -18,13 +19,21 @@ function Auth() {
     const onchange = () => {
 
     }
-    const switchMode=()=>{
-        setisSignup((prevSIgnup)=>!prevSIgnup);
+    const switchMode = () => {
+        setisSignup((prevSIgnup) => !prevSIgnup);
         handlepassword(false)
     }
 
-    const handlepassword = ()=>{
-        setshowPassword((prev)=> !prev)
+    const handlepassword = () => {
+        setshowPassword((prev) => !prev)
+    }
+
+    const googleSuccess = async(res)=>{
+        console.log(res);
+    }
+    const googleFailure = (err) =>{
+        console.log(err);
+        console.log("Unsuccessful Sign in");
     }
     return (
         <Container component="main" maxWidth="xs">
@@ -46,16 +55,36 @@ function Auth() {
                         }
                         <Input name='email' label="Email Address" handleChange={onchange} type='email' />
                         <Input name='password' label="Password" handlepassword={handlepassword} type={showPassword ? 'text' : 'password'} />
-                        {isSignUp && <Input name = "confirmpassword" label = "Repeat password" handleChange = {onchange} type = 'password'/>}
+                        {isSignUp && <Input name="confirmpassword" label="Repeat password" handleChange={onchange} type='password' />}
 
                     </Grid>
-                    <Button type = 'submit' fullWidth variant ="contained" color = 'primary' className = {classes.submit}>
-                        {isSignUp?"Sign up" : "Sign in"}
+                    <Button type='submit' fullWidth variant="contained" color='primary' className={classes.submit}>
+                        {isSignUp ? "Sign up" : "Sign in"}
                     </Button>
-                    <Grid container justify = "flex-end">
+                    <GoogleLogin
+                        clientId="659895284133-if6ts2ldcqq2k2r030gkj5lmsao6uu4l.apps.googleusercontent.com"
+                        render={(renderProps) => (
+                            <Button
+                                className={classes.googleButton}
+                                color='primary'
+                                fullWidth
+                                onClick={renderProps.onClick}
+                                disabled={renderProps.disabled}
+                                startIcon={<Icon />}
+                                variant='contained'
+                                >
+                                    Sign in with Google
+                                </Button>
+                        )}
+                        onSuccess={googleSuccess}
+                        onFailure={googleFailure}
+                        cookiePolicy="single_host_origin"
+                    />
+                
+                    <Grid container justify="flex-end">
                         <Grid item>
-                            <Button onClick = {switchMode}>
-                                {isSignUp ? "Already have an account? Sign in":"Don't have an account? Sign up"}
+                            <Button onClick={switchMode}>
+                                {isSignUp ? "Already have an account? Sign in" : "Don't have an account? Sign up"}
                             </Button>
                         </Grid>
 
