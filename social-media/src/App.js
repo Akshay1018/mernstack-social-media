1f9b21711b8f90ee5d69e6581643b40b94402ccb
akshay1018 <ak715173@gmail.com>
2021-06-17 08:53:24 +0530

    added Home and navbar component

--- social-media/src/App.js
+++ social-media/src/App.js
@@ -1,57 +1,30 @@
-import React, { useEffect, useState } from 'react'
-import { useDispatch } from 'react-redux';
-import {Container, Grow, Grid } from '@material-ui/core';
-import { getPosts } from './actions/posts'
-import memories from './components/images/memory.jpg';
-import Posts from './components/posts';
-import Form from './components/form';
-import useStyles from './styles';
+import React from 'react'
+
+import { Container } from '@material-ui/core';
+
 import NavBar from './components/NavBar/Navbar'
+import Home from './components/Home/Home';
 
 // import Register from './components/Register.js';
 // import Login from './components/Login'
-// import { Route, BrowserRouter as Router, Switch, Link } from 'react-router-dom';
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
 function App() {
-    const [currentId, setcurrentId] = useState(null);
-    const classes = useStyles();
-    const dispatch = useDispatch();
-   
 
-
-    useEffect(() => {
-        dispatch(getPosts());
-    }, [dispatch])
     return (
-        <Container maxWidth="lg">
-           <NavBar/>
-            <Grow in>
-                <Container>
-                    <Grid  container  justify='space-between' alignItems='stretch' spacing={3}>
-                        <Grid item xs={12} sm={7}>
-                            <Posts setcurrentId={setcurrentId} />
-                        </Grid>
-                        <Grid item xs={12} sm={4}>
-                            <Form currentId={currentId} setcurrentId={setcurrentId} />
-                        </Grid>
+        <>
+            <BrowserRouter>
+                <Container maxWidth="lg">
+                    <NavBar />
+                    <Switch>
+                        <Route exact path = '/' component = {Home}/>
+                    </Switch>
+                    <Home />
 
-                    </Grid>
                 </Container>
-            </Grow>
+            </BrowserRouter>
 
-        </Container>
-        // <Router>
-        //     <Switch>
-        //         <Route exact path='/register' component={Register} />
-        //         <Route exact path='/login' component={Login} />
-        //         <Route component={
-        //             () => (
-        //                 <h1>Not Found.
-        //                         Go Back <Link to='/'>Home Page</Link>
-        //                 </h1>
-        //             )
-        //         } />
-        //     </Switch>
-        // </Router>
+        </>
+
 
     )
 }
--- social-media/src/components/NavBar/Navbar.js
+++ social-media/src/components/NavBar/Navbar.js
@@ -1,17 +1,36 @@
 import React from 'react'
-import { AppBar, Typography } from '@material-ui/core'
-import useStyles from '../../styles';
+import { AppBar, Typography, Toolbar, Button ,Avatar} from '@material-ui/core'
+import useStyles from './style';
 import memories from '../../components/images/memory.jpg';
+import { Link } from 'react-router-dom'
 const Navbar = () => {
     const classes = useStyles();
+    const user = null;
     return (
         <AppBar className={classes.appBar} position="static" color="inherit">
-            <Typography className={classes.heading} variant="h2" align='center'>
+            <div className={classes.brandContainer}>
+                <Typography component={Link} to='/' className={classes.heading} variant="h2" align='center'>
 
 
-                Memoir
-            </Typography>
-            <img className={classes.image} src={memories} alt='memories' height='60' />
+                    Memoir
+                </Typography>
+                <img className={classes.image} src={memories} alt='memories' height='60' />
+
+            </div>
+            <Toolbar className={classes.toolbar}>
+                {user ? (
+                    <div className={classes.profile}>
+                        <Avatar className={classes.purple} alt={user.resukt.name} src={user.result.imageURL}>
+                            {user.resukt.name.charAt(0)}
+                        </Avatar>
+                        <Typography className={classes.userName} variant="h6" >{user.result.name}</Typography>
+                        <Button className = {classes.logout} varient = "contained" color ='secondary'>Logout</Button>
+
+                    </div>
+                ) : (
+                    <Button component = {Link} to = '/auth' varient = "contained" color="primary">Sign_in</Button>
+                )}
+            </Toolbar>
 
         </AppBar>
     )
