3ac03b9c6046e558cf0a1e8dd8e7942dff0c0e8d
akshay1018 <ak715173@gmail.com>
2021-07-02 16:40:25 +0530

    added search functionality

--- social-media/src/App.js
+++ social-media/src/App.js
@@ -5,30 +5,30 @@ import { Container } from '@material-ui/core';
 import NavBar from './components/NavBar/Navbar'
 import Home from './components/Home/Home';
 import PostDetails from './components/PostDetails/PostDetails';
-import { Route, BrowserRouter, Switch, Redirect } from 'react-router-dom';
+import { Route, BrowserRouter, Switch,Redirect } from 'react-router-dom';
 import Auth from './components/auth/Auth';
 function App() {
     const user = JSON.parse(localStorage.getItem('token'))
     return (
+        
+            <BrowserRouter>
+                <Container maxWidth="xl">
+                    <NavBar />
+                    <Switch>
+                        <Route exact path = '/' component = {()=> <Redirect to ="/posts"/>}/>
+                        <Route exact path = '/posts' component = {Home}/>
+                        <Route exact path = '/posts/search' component = {Home}/>
+                        <Route exact path = '/posts/:id' component = {PostDetails}/>
 
-        <BrowserRouter>
-            <Container maxWidth="xl">
-                <NavBar />
-                <Switch>
-                    <Route exact path='/' component={() => <Redirect to="/posts" />} />
-                    <Route exact path='/posts' component={Home} />
-                    <Route exact path='/posts/search' component={Home} />
-                    <Route exact path='/posts/:id' component={PostDetails} />
+                        <Route exact path = '/auth' component = {Auth}/>
 
-                    <Route exact path='/auth' component={() => (!user ? <Auth /> : <Redirect to='/posts' />)} />
+                    </Switch>
+                  
 
-                </Switch>
+                </Container>
+            </BrowserRouter>
 
-
-            </Container>
-        </BrowserRouter>
-
-
+       
 
 
     )
--- social-media/src/components/Home/Home.js
+++ social-media/src/components/Home/Home.js
@@ -8,6 +8,7 @@ import { getPosts } from '../../actions/posts'
 import { useDispatch } from 'react-redux';
 import Paginate from '../Pagination/Pagination'
 import { classes } from 'istanbul-lib-coverage';
+import useStyles from './styles';
 
 function useQuery() {
     return new URLSearchParams(useLocation().search);
@@ -17,12 +18,30 @@ const Home = () => {
     const dispatch = useDispatch();
     const query = useQuery();
     const history = useHistory();
+    const classes = useStyles();
 
     const page = query.get('page') || 1;
     const serchQuery = query.get('serchQuery')
+    const [search, setSearch] = useState('');
+    const [tag, setTag] = useState([]);
+
     useEffect(() => {
         dispatch(getPosts());
     }, [currentId, dispatch]);
+
+    const handleKeyPress = (e) => {
+        if (e.keyCode === 13) {
+
+        }
+    }
+    const handleAdd = (tag) => {
+        setTag([...tag, tag])
+    }
+    const handleDelete = (tagTodelete) => {
+        setTag(tag.filter((tag) => tag !== tagTodelete));
+
+    }
+
     return (
 
         <Grow in>
@@ -32,11 +51,23 @@ const Home = () => {
                         <Posts setcurrentId={setcurrentId} />
                     </Grid>
                     <Grid item xs={12} sm={6} md={3}>
-                        <AppBar className = {classes.appBarSearch} position = "static" color = 'inherit'>
+                        <AppBar className={classes.appBarSearch} position="static" color='inherit'>
                             <TextField
-                                name = "search"
-                                variant= "outlined"
-                                label = "Search posts"
+                                name="search"
+                                variant="outlined"
+                                label="Search posts"
+                                fullWidth
+                                onKeyPress={handleKeyPress}
+                                value={search}
+                                onchange={(e) => setSearch(e.target.value)}
+                            />
+                            <ChipInput
+                                style={{ margin: '10px 0' }}
+                                value={tag}
+                                onAdd={handleAdd}
+                                onDelete={handleDelete}
+                                variant="outlined"
+                                label="Search Tags"
                             />
                         </AppBar>
                         <Form currentId={currentId} setcurrentId={setcurrentId} />
